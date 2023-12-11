e2a51f184d04d62e69af1dd1f60c8c83bfc601b8
michaelchan3851 <michaelchan3851@gmail.com>
2023-12-11 22:34:34 +0800

    update

--- ejtos-react_budget_app/src/App.js
+++ ejtos-react_budget_app/src/App.js
@@ -1,80 +1,68 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
+// Code to import Budget.js
+import Budget from "./components/Budget";
 
 // Add code to import the other components here under
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
-import AllocationForm from './components/AllocationForm';
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseItem from "./components/ExpenseItem";
+import AllocationForm from "./components/AllocationForm";
+import Currency from "./components/Currency";
 
+import { AppProvider } from "./context/AppContext";
 
-
-import { AppProvider } from './context/AppContext';
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                            // Budget component
-                             <div className='col-sm'>
-                             <Budget />
-                             </div>
-                        }        
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Company's Budget Allocation</h1>
+        <div className="row mt-3">
+          {/* Add Budget component here */}
+          <div className="col-sm">
+            <Budget />
+          </div>
 
-                        {
-                            /* Add Remaining component here*/
-                            //Remaining component
-                            <div className='col-sm'>
-                            <Remaining />
-                            </div>
-                        }        
+          {/* Add Remaining component here*/}
+          <div className="col-sm">
+            <Remaining />
+          </div>
 
-                        {
-                            /* Add ExpenseTotal component here */                 
-                            //ExpenseTotal component
-                            <div className='col-sm'>
-                            <ExpenseTotal />
-                            </div>
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                            <div className='row '>
-                                <div className='col-sm'>
-                                    <ExpenseList />
-                                </div>
-                            </div>
-                        }         
+          {/* Add ExpenseTotal component here */}
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
 
-                        {
-                            /* Add ExpenseItem component here */
-                            <div className='row mt-3'>
-                            <div className='col-sm'>
-                                <ExpenseItem/>
-                            </div>
-                        </div>
-                        }        
+          {/* Add Currency component here */}
+          <div className="col-sm">
+            <Currency />
+          </div>
+        </div>
 
-                        {
-                            /* Add AllocationForm component here under */
-                            <div className='row mt-3'>
-                            <div className='col-sm'>
-                                <AllocationForm/>
-                            </div>
-                        </div>
-                        }        
+        {/* Add Allocation heading here */}
+        <h3 className="mt-3">Allocation</h3>
 
-                </div>
-            </div>
-        </AppProvider>
-    );
+        {/* Add ExpenseList component here */}
+        <div className="row ">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+
+        {/* Add Change Allocation heading here */}
+        <h3 className="mt-3">Change Allocation</h3>
+
+        {/* Add AllocationForm component here under */}
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AllocationForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
 };
+
 export default App;
--- ejtos-react_budget_app/src/components/ExpenseItem.js
+++ ejtos-react_budget_app/src/components/ExpenseItem.js
@@ -1,39 +1,72 @@
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: -10, // Decrease by 10
+    };
 
-    }
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>£{props.cost}</td>
+      <td>
+        <button
+          className="btn btn-success btn-sm rounded-circle"
+          onClick={() => increaseAllocation(props.name)}
+          style={{ marginRight: "5px" }}
+        >
+          +
+        </button>
+      </td>
+      <td>
+        {" "}
+        <button
+          className="btn btn-danger btn-sm rounded-circle"
+          onClick={() => decreaseAllocation(props.name)}
+        >
+          -
+        </button>
+      </td>
+      <td>
+        <TiDelete
+          size="1.5em"
+          onClick={handleDeleteExpense}
+          style={{ cursor: "pointer", color: "red" }}
+        />
+      </td>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
-
